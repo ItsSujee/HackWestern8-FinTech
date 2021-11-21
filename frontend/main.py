820295ba6b44e6dce76fc2c34960b2d34926af0d
Sujeethan Vigneswaran <sujeethan.vigneswaran@gmail.com>
2021-11-21 15:21:14 -0500

    Done

--- frontend/main.py
+++ frontend/main.py
@@ -1,7 +1,10 @@
 import requests
 from flask import Flask, render_template, request
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/", methods=['GET'])
 def homepage():
@@ -10,15 +13,15 @@ def homepage():
 @app.route('/', methods=['POST'])
 def my_form_post():
     text = request.form["input"]
-    url = 'http://bb74-34-82-224-201.ngrok.io/query'
+    url = 'http://be86-64-228-155-213.ngrok.io/query'
     myobj = {'query': text}
     x = requests.post(url, data=myobj)
     x = x.json()
-    confidence = round(float(x["confidence"]), 4)
+    confidence = str(round(float(x["confidence"]), 4))
     if x["prediction"] == "real":
-        return render_template("real.html", confidence=abs(100-int(confidence)))
+        return render_template("real.html", confidence=confidence)
     else:
-        return render_template("fake.html", confidence=abs(int(confidence)))
+        return render_template("fake.html", confidence=confidence)
 
 if __name__ == "__main__":
     app.run(debug=True)
